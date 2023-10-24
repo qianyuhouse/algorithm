ebbca45cd8959c4e1fbdeb66a30504d3f495a5b7
Calvin.Fu <qianyuhouse@163.com>
2023-10-24 15:44:22 +0800

    feat: add custom promise

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-import { chunk } from "./chunk";
-
-console.log(chunk([1, 2, 3, 4, 5], 2));
+import "./promise";
--- src/promise.ts
+++ src/promise.ts
@@ -9,6 +9,7 @@
 // 如果调用 then 时，promise 已经成功，则执行onFulfilled，参数是promise的value；
 // 如果调用 then 时，promise 已经失败，那么执行onRejected, 参数是promise的reason；
 // 如果 then 中抛出了异常，那么就会把这个异常作为参数，传递给下一个 then 的失败的回调onRejected；
+// https://juejin.cn/post/6850037281206566919
 
 export enum CustomPromiseStatus {
   Pending = "PENDING",
@@ -16,22 +17,241 @@ export enum CustomPromiseStatus {
   Rejected = "REJECTED",
 }
 
+export type CustomPromiseResolve<T> = (res?: T) => void;
+export type CustomPromiseReject = (err?: unknown) => void;
+export type CustomPromiseExecutor<T> = (
+  resolve: CustomPromiseResolve<T>,
+  reject: CustomPromiseReject
+) => void;
+
+const customResolve = (
+  promise: CustomPromise<any>,
+  x: any,
+  resolve: CustomPromiseResolve<any>,
+  reject: CustomPromiseReject
+) => {
+  if (promise === x) {
+    return reject(
+      new TypeError("Chaining cycle detected for promise #<Promise>")
+    );
+  }
+  let called = false;
+  let then =
+    (typeof x === "object" && x !== null) || typeof x === "function"
+      ? x.then
+      : undefined;
+  if (typeof then === "function") {
+    try {
+      then.call(
+        x,
+        (y: any) => {
+          if (called) return;
+          called = true;
+          customResolve(promise, y, resolve, reject);
+        },
+        (r: unknown) => {
+          if (called) return;
+          called = true;
+          reject(r);
+        }
+      );
+    } catch (e) {
+      if (called) return;
+      called = true;
+      reject(e);
+    }
+  } else {
+    resolve(x);
+  }
+};
+
+// use Promise.race to stop another promise
+// function wrap(promise) {
+//   let abort;
+//   let newPromise = new Promise((resolve, reject) => { // defer
+//       abort = reject;
+//   });
+//   let p = Promise.race([promise, newPromise]);
+//   p.abort = abort;
+//   return p;
+// }
+
 export class CustomPromise<T> {
   status: CustomPromiseStatus = CustomPromiseStatus.Pending;
   value?: T;
-  reason?: string;
-  constructor() {
+  reason?: unknown;
+  onResolvedCallbacks: CustomPromiseResolve<T>[] = [];
+  onRejectedCallbacks: CustomPromiseReject[] = [];
+
+  static resolve(data?: any) {
+    return new CustomPromise((resolve) => {
+      resolve(data);
+    });
+  }
+
+  static reject(reason?: unknown) {
+    return new Promise((resolve, reject) => {
+      reject(reason);
+    });
+  }
+
+  static all(promises: CustomPromise<any>[]) {
+    if (!Array.isArray(promises)) {
+      const type = typeof promises;
+      return new TypeError(`TypeError: ${type} ${promises} is not iterable`);
+    }
+
+    return new Promise((resolve, reject) => {
+      let resultArr: any[] = [];
+      let orderIndex = 0;
+      const processResultByKey = (value: any, index: number) => {
+        resultArr[index] = value;
+        if (++orderIndex === promises.length) {
+          resolve(resultArr);
+        }
+      };
+      for (let i = 0; i < promises.length; i++) {
+        let value = promises[i];
+        if (value && typeof value.then === "function") {
+          value.then((value) => {
+            processResultByKey(value, i);
+          }, reject);
+        } else {
+          processResultByKey(value, i);
+        }
+      }
+    });
+  }
+
+  static race(promises: CustomPromise<any>[]) {
+    return new CustomPromise((resolve, reject) => {
+      // 一起执行就是for循环
+      for (let i = 0; i < promises.length; i++) {
+        let val = promises[i];
+        if (val && typeof val.then === "function") {
+          val.then(resolve, reject);
+        } else {
+          // 普通值
+          resolve(val);
+        }
+      }
+    });
+  }
+
+  constructor(executor: CustomPromiseExecutor<T>) {
     this.status = CustomPromiseStatus.Pending;
     this.value = undefined;
     this.reason = undefined;
-  }
+    this.onResolvedCallbacks = [];
+    this.onRejectedCallbacks = [];
 
-  resolve(value?: T) {
-    if (this.status === CustomPromiseStatus.Pending) {
-      this.status = CustomPromiseStatus.Fulfilled;
-      this.value = value;
+    // make a resolve function
+    let resolve = (value?: T): any => {
+      if (value instanceof Promise) {
+        // continue execute
+        return value.then(resolve, reject);
+      }
+      if (this.status === CustomPromiseStatus.Pending) {
+        this.status = CustomPromiseStatus.Fulfilled;
+        this.value = value;
+        this.onResolvedCallbacks.forEach((fn) => fn());
+      }
+    };
+
+    // make a reject funciton
+    let reject = (reason?: unknown) => {
+      if (this.status === CustomPromiseStatus.Pending) {
+        this.status = CustomPromiseStatus.Rejected;
+        this.reason = reason;
+        this.onRejectedCallbacks.forEach((fn) => fn());
+      }
+    };
+
+    try {
+      // execute
+      executor(resolve, reject);
+    } catch (e) {
+      reject(e);
     }
   }
 
-  reject(reason?: string) {}
+  then(
+    onFulfilled?: CustomPromiseResolve<T>,
+    onRejected?: CustomPromiseReject
+  ) {
+    // set default resolve
+    const _onFulfilled: CustomPromiseResolve<T> =
+      typeof onFulfilled === "function" ? onFulfilled : (v) => v;
+    // set default reject
+    const _onRejected: CustomPromiseReject =
+      typeof onRejected === "function"
+        ? onRejected
+        : (err) => {
+            throw err;
+          };
+
+    // each then called will return a new promise
+    let promise = new CustomPromise((resolve, reject) => {
+      if (this.status === CustomPromiseStatus.Fulfilled) {
+        setTimeout(() => {
+          try {
+            customResolve(promise, _onFulfilled(this.value), resolve, reject);
+          } catch (e) {
+            reject(e);
+          }
+        }, 0);
+      }
+
+      if (this.status === CustomPromiseStatus.Rejected) {
+        setTimeout(() => {
+          try {
+            customResolve(promise, _onRejected(this.reason), resolve, reject);
+          } catch (e) {
+            reject(e);
+          }
+        }, 0);
+      }
+
+      // if resolve is an async function, push to tasks
+      if (this.status === CustomPromiseStatus.Pending) {
+        this.onResolvedCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              customResolve(promise, _onFulfilled(this.value), resolve, reject);
+            } catch (e) {
+              reject(e);
+            }
+          }, 0);
+        });
+
+        this.onRejectedCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              customResolve(promise, _onRejected(this.reason), resolve, reject);
+            } catch (e) {
+              reject(e);
+            }
+          }, 0);
+        });
+      }
+    });
+    return promise;
+  }
+
+  catch(onRejected?: CustomPromiseReject) {
+    return this.then(undefined, onRejected);
+  }
+
+  finally(callback: CustomPromiseResolve<T>) {
+    return this.then(
+      (value) => {
+        return CustomPromise.resolve(callback()).then(() => value);
+      },
+      (reason) => {
+        return CustomPromise.resolve(callback()).then(() => {
+          throw reason;
+        });
+      }
+    );
+  }
 }
